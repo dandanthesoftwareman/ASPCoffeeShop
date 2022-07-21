0fdcd4a2a0664c6b74a01ac9fcb6aacca848983d
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-07-21 15:58:36 -0400

    first commit

---
+++ CoffeeShopregistrationLab/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using CoffeeShopregistrationLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopregistrationLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult NewShopForm()
+        {
+            return View();
+        }
+        public IActionResult NewShopResult(NewShop n)
+        {
+            return View(n);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopregistrationLab/Models/NewShop.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeShopregistrationLab.Models
+{
+    public class NewShop
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
